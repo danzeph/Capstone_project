1b79b3f628d0ad43a76945aed36616cfdcd972cb
danzeph <danzeph00@gmail.com>
2026-02-28 21:48:31 +0000

    updated serializers.py

--- inventory_management/inventory/serializers.py
+++ inventory_management/inventory/serializers.py
@@ -15,3 +15,22 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 
+class InventoryItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InventoryItem
+        fields = '__all__'
+        read_only_fields = ('user', 'date_added', 'last_updated')
+
+    def validate(self, data):
+        """Object level validation to validate quantity and price"""
+        if data['quantity'] < 0:
+            raise serializers.ValidationError("Quantity cannot be negative")
+        if data['price'] <= 0:
+            raise serializers.ValidationError("Price must be greater than zero")
+        return data
+    
+
+class InventoryChangeLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InventoryChangeLog
+        fields = '__all__'
