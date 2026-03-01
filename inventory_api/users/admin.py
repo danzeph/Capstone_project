846766d920e7ac3a6c2ade40ecc4737f9310b5ab
danzeph <danzeph00@gmail.com>
2026-03-01 12:44:20 +0000

    added admin.py files to view custom user

--- inventory_api/users/admin.py
+++ inventory_api/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class Danamaz(DjangoUserAdmin):
+    model = User
+
+    fieldsets = DjangoUserAdmin.fieldsets
+    add_fieldsets = DjangoUserAdmin.add_fieldsets
+    list_display = ("id", "username", "email", "is_staff", "is_active")
+    search_fields = ("username", "email")
+    ordering = ("id",)
