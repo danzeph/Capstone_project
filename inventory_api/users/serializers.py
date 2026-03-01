ebe63d7369a78a6ef40807843e43868e019d9310
danzeph <danzeph00@gmail.com>
2026-03-01 11:59:39 +0000

    added my serializers.py and updated views.py

---
+++ inventory_api/users/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+# from users.models import User
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    """User serializer with password_confirm added for validation"""
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password','password_confirm']
+        
+
+    def validate(self,data):
+            if data['password'] != data['password_confirm']:
+                raise serializers.ValidationError({"password": "Passwords do not match"})
+            return data
+
+    def create(self, validated_data):
+            validated_data.pop('password_confirm')
+            user = User.objects.create_user(**validated_data)
+            return user
--- inventory_api/users/views.py
+++ inventory_api/users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from django.contrib.auth import get_user_model
+from users.serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        """Users can only access their own account; admin sees everyone"""
+        user = self.request.user
+        if user.is_staff:
+            return User.objects.all()
+        if user.is_authenticated:
+            return User.objects.filter(id=user.id)
+        return User.objects.none()
+    
+    def get_permissions(self):
+        """Allow anyone to register"""
+        if self.action == 'create':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
